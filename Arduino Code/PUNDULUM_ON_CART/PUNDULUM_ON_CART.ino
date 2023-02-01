18e00ad5dc9cb41bd0c344370af798f2fd37f520
Ronald Stivel Melo Cardenas <ronald9412@gmail.com>
2023-01-31 20:02:56 -0500

    Se implemento algoritmo PID, funcional, falta acoplar el control de las variables por medio de IOT

--- Arduino Code/PUNDULUM_ON_CART/PUNDULUM_ON_CART.ino
+++ Arduino Code/PUNDULUM_ON_CART/PUNDULUM_ON_CART.ino
@@ -5,9 +5,10 @@
 
 ///--------------------PID constants-------------------
 float angle=0.0;
-float kp=15; //Mine 
-float ki=0.405; //Mine
-float kd=1780; //Mine 
+float kp=15.01; //Mine 
+float ki=0.403; //Mine
+float angle_ki = 3;
+float kd=1778.9; //Mine 
 
 float angle_setpoint = 0;         
 float PID_p, PID_i, PID_d, PID_total;
@@ -21,7 +22,6 @@ int period = 50;  //Refresh rate period of the loop is 50ms
 //------------------------------------------------------
 
 int speed = 65536;
-int startSpeed = 0;
 
 void moving();
 void PIDcontrol();
@@ -38,6 +38,7 @@ void setup() {
 void loop() {
   ReadRawAngle();
   checkQuadrant();  
+  checkMagnetPresence();
 
   // // if (Firebase.ready()){
   // if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)) {
@@ -78,16 +79,15 @@ void PIDcontrol() {
   if (millis() > currentTime+period){
 
     angle = correctAngle();
-    if(angle > 298 && angle < 360) angle = map(angle,360,298,0,-60);
-    // else if(angle > 0 && angle < 60){
-    //   angle = map(angle,0,60,0,-60);
-    // }
+    if(angle > 296 && angle < 360) angle = map(angle,360,296,0,-62);
+    if(angle > 0 && angle < 61) angle = map(angle,0,61,0,61);
     Serial.println("Angulo: "+String(angle));
     angle_error = angle_setpoint - angle;
     PID_p = kp * angle_error; //**** KP
     angle_diference = angle_error - angle_previous_error;
     PID_d = kd*((angle_error-angle_previous_error)/period);  //**** KD
-    if(-3 < angle_diference && angle_error < 3)
+    // PID_i = PID_i + (ki * angle_error); //***** KI
+    if(-angle_ki < angle_diference && angle_error < angle_ki)
     {
       PID_i = PID_i + (ki * angle_error);
     }
@@ -98,8 +98,9 @@ void PIDcontrol() {
 
     PID_total = PID_p + PID_i + PID_d;  
     
-    Serial.println("PID PRE: " + String(PID_total));    
-    PID_total = map(PID_total, 0, 100, 0, speed-startSpeed);
+    Serial.println("PID PRE: " + String(PID_total));   
+    // if(PID_total < 0) map(PID_total, -100, 100, 0, speed);
+    PID_total = map(PID_total, 0, 75, 0, speed);
     
   
     if(PID_total < -speed) PID_total = -speed;
@@ -109,15 +110,15 @@ void PIDcontrol() {
 
     Serial.println("Error PRE: "+ String(angle_error));
     if(angle_error < -0.51){
-      forward(abs(PID_total)+startSpeed);
+      forward(abs(PID_total));
     }else if(angle_error > 0.51){
-      backward(abs(PID_total)+startSpeed);
+      backward(abs(PID_total));
     } 
     else if( (angle_error > -0.5 && angle_error < 0.5) || angle_error < -55 || angle_error > 55){
       brake();
     }
-    
     angle_previous_error = angle_error;
+
     
     Serial.println("Error POS: "+ String(angle_error));
     
